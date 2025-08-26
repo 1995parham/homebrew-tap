470c4b15a04dd4dc8e12615d186585c8abf156d0
Parham Alvani <parham.alvani@gmail.com>
2025-08-26 20:47:17 +0000

    feat: upgrade dijo formula

--- dijo.rb
+++ dijo.rb
@@ -2,7 +2,7 @@ class Dijo < Formula
   desc "scriptable, curses-based, digital habit tracker"
   homepage "https://github.com/nerdypepper/dijo"
   license "MIT"
-  version "v0.3.2"
+  version "v0.4.0"
 
   sha256 "bc8bbdf78a157fc468c45e4d72e995a14dfba9872bbb5a48b87aa1e2a8284a96"
   url "https://github.com/1995parham/dijo/releases/download/#{version}/dijo-aarch64-apple"
