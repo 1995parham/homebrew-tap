844f13184f13bd938171e49ae9e9086a3f6f0eb1
Parham Alvani <parham.alvani@gmail.com>
2025-08-26 23:24:44 +0000

    feat: update dijo version

--- dijo.rb
+++ dijo.rb
@@ -4,7 +4,7 @@ class Dijo < Formula
   license "MIT"
   version "v0.4.1"
 
-  sha256 "c36b2341e5e5f302c11822429a78724bd3fcef855642cf1a0c3d105734fa023a"
+  sha256 "955fcf82d4b570c7e75aa2ee280e317a6d5d52e26b82140dfb7a5804e32b7b17"
   url "https://github.com/1995parham/dijo/releases/download/#{version}/dijo-aarch64-apple"
 
   def install
