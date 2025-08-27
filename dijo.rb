01870527d6491668765d92374df8f468f404ce56
Parham Alvani <parham.alvani@gmail.com>
2025-08-27 21:04:12 +0000

    feat: update dijo

--- dijo.rb
+++ dijo.rb
@@ -2,9 +2,9 @@ class Dijo < Formula
   desc "scriptable, curses-based, digital habit tracker"
   homepage "https://github.com/1995parham/dijo"
   license "MIT"
-  version "v0.4.2"
+  version "v0.4.4"
 
-  sha256 "415359122faddeee5adca5f8ca3969e3b92b698514a360f26dd38a4dfb544f5e"
+  sha256 "fd32a6ed71795ebab2ede1b6fd749c04048f980cafea7826c00e5f60d1bcb1b0"
   url "https://github.com/1995parham/dijo/releases/download/#{version}/dijo-aarch64-apple"
 
   def install
