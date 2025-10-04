b6663b4d5e15ece61d3b9d0a1093004bb32cc771
Parham Alvani <parham.alvani@gmail.com>
2025-10-04 04:22:57 +0000

    fix: correct binaries hash

--- dijo.rb
+++ dijo.rb
@@ -5,12 +5,12 @@ class Dijo < Formula
   version "v0.4.7"
 
   on_arm do
-    sha256 "sha256:5a7f2be8215b1c42e871dd86b3690f74b16decb9051578bc984024e481d03db0"
+    sha256 "5a7f2be8215b1c42e871dd86b3690f74b16decb9051578bc984024e481d03db0"
     url "https://github.com/1995parham/dijo/releases/download/#{version}/dijo-aarch64-apple-darwin.tar.gz"
   end
 
   on_intel do
-    sha256 "sha256:e2bdb91f253f7a2c848c366992a88c5aee5ae5c462590282eb0f644f67013ab7"
+    sha256 "e2bdb91f253f7a2c848c366992a88c5aee5ae5c462590282eb0f644f67013ab7"
     url "https://github.com/1995parham/dijo/releases/download/#{version}/dijo-x86_64-apple-darwin.tar.gz"
   end
 
