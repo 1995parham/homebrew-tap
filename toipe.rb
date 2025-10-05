14b10dad17be250f59332a903cb43fefb62463d5
Parham Alvani <parham.alvani@gmail.com>
2025-10-05 04:42:27 +0000

    feat: update version and the way of installation

--- toipe.rb
+++ toipe.rb
@@ -1,8 +1,8 @@
 class Toipe < Formula
   desc "yet another typing test, but crab flavoured"
   homepage "https://github.com/Samyak2/toipe"
-  sha256 "31e4c7679487425254ad90bbc4d14a9bd55af6c6a20cce0b3f8eaa52fffe6bf7"
-  url "https://github.com/Samyak2/toipe/archive/refs/tags/v0.5.0.tar.gz"
+  version "0.5.0"
+  url "https://github.com/Samyak2/toipe/archive/refs/tags/v#{version}.tar.gz"
   license "MIT"
 
   depends_on "rust" => :build
